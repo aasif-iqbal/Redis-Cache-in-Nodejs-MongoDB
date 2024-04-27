6396b336a17adc7c6ba77339c98649e7955b4882
aasif-iqbal <aasif.iqbal3000@gmail.com>
2024-04-27 21:49:07 +0530

    setup server

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log('hello');
+import express from "express";
+
+const app = express();
+
+const PORT = 3000;
+
+app.use(express.json());
+// This is a built-in middleware function in Express. It parses incoming requests with urlencoded payloads and is based on body-parser.
+app.use(express.urlencoded({extended:true}));
+
+app.get('/', (req, res)=>{
+    res.send('working...')
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on Port ${PORT}`);
+})
