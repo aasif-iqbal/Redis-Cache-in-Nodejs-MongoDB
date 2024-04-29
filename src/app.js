1cbcba4542f4004b73a47cd51ab6a1c193df10a7
aasif-iqbal <aasif.iqbal3000@gmail.com>
2024-04-30 01:01:27 +0530

    redis cache added

--- src/app.js
+++ src/app.js
@@ -3,12 +3,16 @@ import 'dotenv/config';
 import connectionDB from "./config.js";
 import UserRoute from "./routes/userRoute.js";
 import { faker } from '@faker-js/faker';
+import * as redis from "redis";
 
 process.on('uncaughtException', (e) => {
     console.log('Error',e);
 });
-                                                                                                                          
-  
+
+
+let redisClient;
+
+
 const app = express();
 
 connectionDB();
@@ -20,13 +24,14 @@ const PORT = process.env.PORT;
 app.use(express.json());
 // This is a built-in middleware function in Express. It parses incoming requests with urlencoded payloads and is based on body-parser.
 app.use(express.urlencoded({extended:true}));
-app.use("/api", UserRoute);
+app.use("/users", UserRoute);
 
 app.get('/', (req, res)=>{
     res.send('working...')
 });
 
 
+// The "Maximum response size reached" error in Postman 
 
 app.listen(PORT, () => {
     console.log(`Server running on Port ${PORT}`);
@@ -34,4 +39,13 @@ app.listen(PORT, () => {
     console.log(e);
 });
 
+(async () => {
+    redisClient = redis.createClient();
+  
+    redisClient.on("error", (error) => console.error(`Error : ${error}`));
+  
+    await redisClient.connect();
+    console.log('Redis connected..');
+  })();                                         
+    
 // export default app;
--- src/routes/UserRoute.js
+++ src/routes/UserRoute.js
@@ -1,42 +1,74 @@
-// blog_app/routes/ArticleRouter.js
 import express from "express";
 import UserModel from "../models/user.js";
-import { faker } from '@faker-js/faker';
+import { faker } from "@faker-js/faker";
+import * as redis from "redis";
 
 const router = express.Router();
- 
-router.get('/test', async(request,response) => {
-    response.send('okk');
+
+router.get("/test", async (request, response) => {
+  response.send("okk");
 });
 
-router.get('/userInfo', async(request,response) => {
-    try {
-        const user_data = await UserModel.find({});
-        return response.json(user_data);    
-    } catch (error) {
-        console.log(err);
+const _createRedisClient = async () => {
+  const client = await redis.createClient();
+
+  client.on("error", (err) => {
+    console.error("Redis connection error:", err);
+  });
+
+  return client;
+};
+
+router.get("/", async (request, response) => {
+  try {
+    const redisClient = await _createRedisClient();
+    await redisClient.connect();
+    //get data from cache-memory
+    let redisCache = await redisClient.get(JSON.stringify("my_key"));
+    
+    // console.log(redisCache);
+
+    if (redisCache !== null) {
+      // fetch from redis-cache
+      const user_data = JSON.parse(redisCache);
+      console.log('from cache');
+      return response.json(user_data);
+    } else {
+      // if cache is null(missing) - Fetch from database
+
+      const user_data = await UserModel.find({});
+
+      // set quiz data in redis cache with unique key
+      await redisClient.set(
+        JSON.stringify("my_key"),
+        JSON.stringify(user_data)
+      );
+      console.log('from database');
+      return response.json(user_data);
     }
+  } catch (error) {
+    console.log(error);
+  }
 });
 
-router.post('/userInfo', async(request, response) => {
-// 1 million =  10,00,000
-    try{
-        const fakeUserRecords = Array.from({length:100000}, () => ({            
-                userId: faker.string.uuid(),    
-                username: faker.internet.userName(),
-                email: faker.internet.email(),
-                avatar: faker.image.avatar(),
-                password: faker.internet.password(),
-                birthdate: faker.date.birthdate(),
-                registeredAt: faker.date.past(),
-            }) 
-        );
+router.post("/register", async (request, response) => {
+  // 1 million =  10,00,000
+  try {
+    const fakeUserRecords = Array.from({ length: 1000000 }, () => ({
+      userId: faker.string.uuid(),
+      username: faker.internet.userName(),
+      email: faker.internet.email(),
+      avatar: faker.image.avatar(),
+      password: faker.internet.password(),
+      birthdate: faker.date.birthdate(),
+      registeredAt: faker.date.past(),
+    }));
 
-        await UserModel.insertMany(fakeUserRecords);
-        console.log('data inserted..');
-    }catch(err){
-        console.log(err);
-    }
+    await UserModel.insertMany(fakeUserRecords);
+    console.log("data inserted..");
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 export default router;
