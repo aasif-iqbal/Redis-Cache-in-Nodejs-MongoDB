00bff8a8be150553749af5bda5ddee53df3f0106
aasif-iqbal <aasif.iqbal3000@gmail.com>
2024-05-16 01:17:26 +0530

    update user schema

--- src/models/user.js
+++ src/models/user.js
@@ -7,21 +7,27 @@ const UserSchema = new mongoose.Schema({
   },
   username: {
     type: String,
+    required: true,
   },
   email: {
     type: String,
+    required: true,
   },
   avatar: {
     type: String,
+    required: true,
   },
   password: {
     type: String,
+    required: true,
   },
   birthdate: {
     type: Date,
+    required: true,
   },
   registeredAt: {
     type: Date,
+    required: true,
   },
 });
 
